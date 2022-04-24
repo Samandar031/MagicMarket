643ea7abcc2053f011a5764b54bc3cf2eb6763c5
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-24 09:59:20 +0500

    sign up qismini style tugadi

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,34 @@
-var myModal = document.getElementById(".card_btn");
-var myInput = document.getElementById("myInput");
+let login = document.querySelector(".login_btn_l");
+let create = document.querySelector(".sign_btn_s");
 
-myModal.addEventListener("shown.bs.modal", function () {
-  myInput.focus();
+let loginPage = document.querySelector(".register_login");
+let createPage = document.querySelector(".register_sign");
+
+let a = 1;
+let b = 1;
+
+create.addEventListener("click", function (e) {
+  a++;
+  b++;
+  if (b == 2) {
+    createPage.style.transform = `translateY(${-36 * b}rem)`;
+    b = 1;
+  }
+  if (a == 2) {
+    loginPage.style.transform = `translateY( ${0 * a}rem)`;
+    a = 1;
+  }
+});
+
+login.addEventListener("click", function (e) {
+  a++;
+  b++;
+  if (b == 2) {
+    createPage.style.transform = `translateY(${-13 * b}rem)`;
+    b = 1;
+  }
+  if (a == 2) {
+    loginPage.style.transform = `translateY(${30 * a}rem)`;
+    a = 1;
+  }
 });
