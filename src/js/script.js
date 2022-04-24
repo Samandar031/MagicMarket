7cdf0da4eb4f2d4b7b44f3c04413abc0ac0d9f47
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-24 13:17:04 +0500

    registerga harakat

--- src/js/script.js
+++ src/js/script.js
@@ -32,3 +32,30 @@ login.addEventListener("click", function (e) {
     a = 1;
   }
 });
+
+// popup
+let popupBtn = document.querySelector(".card_btn");
+let popupCard = document.querySelector(".popup");
+let popupClose = document.querySelector(".popup_close");
+
+popupBtn.addEventListener("click", function () {
+  popupCard.style.display = "block";
+});
+
+popupClose.addEventListener("click", function () {
+  popupCard.style.display = "none";
+});
+
+// register
+
+let registerBtn = document.querySelector(".nav_sign");
+let registerCard = document.querySelector(".register");
+let registerClose = document.querySelector(".register_close");
+
+registerBtn.addEventListener("click", function () {
+  registerCard.style.display = "block";
+});
+
+registerClose.addEventListener("click", function () {
+  registerCard.style.display = "none";
+});
